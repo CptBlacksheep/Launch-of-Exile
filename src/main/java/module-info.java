0164c265def46af3862be6f32673e4caecf9c003
CptBlacksheep <andre@reisert-diamant.de>
2022-05-28 17:13:44 +0200

    Added jdk.crypto.cryptoki requirement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,6 @@ module launchofexile {
     requires java.net.http;
     requires com.formdev.flatlaf;
     requires com.fasterxml.jackson.databind;
+    requires jdk.crypto.cryptoki;
     opens com.github.cptblacksheep.launchofexile.datamanagement;
 }
