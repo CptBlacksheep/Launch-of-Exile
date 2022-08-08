36c16b3ec9d363090f37a4e0f4279bab1551c89e
CptBlacksheep <andre@reisert-diamant.de>
2022-08-08 18:40:29 +0200

    version changed to 1.3.1

--- src/main/java/com/github/cptblacksheep/launchofexile/UpdateChecker.java
+++ src/main/java/com/github/cptblacksheep/launchofexile/UpdateChecker.java
@@ -14,7 +14,7 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 
 public class UpdateChecker {
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
 
     private UpdateChecker() {
     }
